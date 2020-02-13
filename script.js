1d7b5dba8c43f7321fa938981e30f436d0bb99de
ms-online <27732357@qq.com>
2020-02-14 00:14:24 +0800

    lesson-5

--- script.js
+++ script.js
@@ -8,15 +8,24 @@ let ticketPrice = +movieSelect.value;
 // 更新座位数及总票价
 function undateSeletedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
-  const selectedSeatsCount = selectedSeats.length;
 
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+  const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+//保存电影索引值和票价
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem("selectedMovieIndex", movieIndex);
+  localStorage.setItem("selectedMoviePrice", moviePrice);
+}
 // 电影下拉框事件监听
 movieSelect.addEventListener("change", e => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   undateSeletedCount();
 });
 // 座位点击事件
