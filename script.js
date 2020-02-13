636fd2e03608e7943a19c37f5688ac7814209ead
ms-online <27732357@qq.com>
2020-02-14 01:44:58 +0800

    lesson-6

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const total = document.getElementById("total");
 const movieSelect = document.getElementById("movie");
 let ticketPrice = +movieSelect.value;
 
+populateUI();
 // 更新座位数及总票价
 function undateSeletedCount() {
   const selectedSeats = document.querySelectorAll(".row .seat.selected");
@@ -21,6 +22,24 @@ function undateSeletedCount() {
 function setMovieData(movieIndex, moviePrice) {
   localStorage.setItem("selectedMovieIndex", movieIndex);
   localStorage.setItem("selectedMoviePrice", moviePrice);
+}
+
+// 获取本地数据并渲染样式
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
+  console.log(selectedSeats);
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add("selected");
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
 }
 // 电影下拉框事件监听
 movieSelect.addEventListener("change", e => {
@@ -39,3 +58,6 @@ container.addEventListener("click", e => {
     undateSeletedCount();
   }
 });
+
+// 设置初始座位和总票价
+undateSeletedCount();
